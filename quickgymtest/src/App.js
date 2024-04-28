4b90ec33bffcb6505093dac5fa2ba32098ebb224
TamannaK24 <149901439+TamannaK24@users.noreply.github.com>
2024-04-28 17:32:51 -0500

    workout tracker

---
+++ quickgymtest/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'; // Import CSS file
+
+const App = () => {
+    return (
+        <div className="menu">
+            <button>Select Exercise</button>
+            <button>Set</button>
+            <button>Rep</button>
+        </div>
+    );
+}
+
+export default App;
